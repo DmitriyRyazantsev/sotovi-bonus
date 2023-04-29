92dc50fef5b989e5a71046bfad52d5765c3de2d3
Dmitry <dmitry.ryazantsev@mail.ru>
2023-04-29 18:28:33 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int score = 100;
+        int replenishment = 1100;
+        int bonus = replenishment / 100;
+
+        if (replenishment <= 310) {
+            bonus = 0;
+        } else {
+            bonus = replenishment / 100;
+        }
+        System.out.println(bonus);
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+    }
+}
---
+++ src/README.md
@@ -0,0 +1,1 @@
+# sotoviy-bonus
